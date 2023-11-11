cafbf2469cae4816b51d6b6836a987d219a167d3
Maja <majkatkaczykk@gmail.com>
2023-11-11 23:27:28 +0100

    add logic to mini game text to ascii

--- ReactNative/ascii-converter/components/mini-game/textToAscii.js
+++ ReactNative/ascii-converter/components/mini-game/textToAscii.js
@@ -1,20 +1,51 @@
-import { Text, View, SafeAreaView, TextInput } from "react-native";
-import React from "react";
+import { Text, View, SafeAreaView, TextInput, Pressable } from "react-native";
+import React, { useState, useEffect } from "react";
 import miniGameStyle from "./miniGameStyle";
 import globalContentStyle from "../../globalStyles/globalContentStyle";
 const TextToAsciiGame = () => {
+  const [randomChar, setRandomChar] = useState("");
+  const [userAnswer, setUserAnswer] = useState("");
+  const [result, setResult] = useState("");
+  useEffect(() => {
+    generateRandomChar();
+  }, []);
+
+  const generateRandomChar = () => {
+    const randomAsciiValue = Math.floor(Math.random() * (125 - 33 + 1)) + 33; // Zakres od 33 do 125
+    setRandomChar(String.fromCharCode(randomAsciiValue));
+    setUserAnswer("");
+    setResult("");
+  };
+  const checkAnswer = () => {
+    const inputValue = userAnswer.trim();
+    const asciiValue = parseInt(inputValue);
+
+    if (!isNaN(asciiValue) && asciiValue === randomChar.charCodeAt(0)) {
+      setResult("Correct!");
+    } else {
+      setResult("Incorrect. Try again.");
+    }
+  };
   return (
     <View style={miniGameStyle.tabContentStyle}>
       <Text style={miniGameStyle.label}>Guess the ASCII decimal number:</Text>
-      <View style={globalContentStyle.outputContainer}></View>
+      <View style={globalContentStyle.outputContainer}>
+        <Text>{randomChar}</Text>
+      </View>
       <Text style={miniGameStyle.label}>Your answer:</Text>
       <SafeAreaView style={miniGameStyle.safeAreaView}>
         <TextInput
-          keyboardType="ascii-capable"
+          onChangeText={setUserAnswer}
+          value={userAnswer}
+          keyboardType="numeric"
           style={globalContentStyle.input}
           maxLength={3}
         />
       </SafeAreaView>
+      <Pressable style={miniGameStyle.convertButton} onPress={checkAnswer}>
+        <Text style={miniGameStyle.buttonText}>Check Answer</Text>
+      </Pressable>
+      <Text style={miniGameStyle.result}>{result}</Text>
     </View>
   );
 };
