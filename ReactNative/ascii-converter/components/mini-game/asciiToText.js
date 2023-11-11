b85772b41631c81378ded5f692db045ae72ce006
Maja <majkatkaczykk@gmail.com>
2023-11-11 23:21:07 +0100

    add logic to mini game ascii to text

--- ReactNative/ascii-converter/components/mini-game/asciiToText.js
+++ ReactNative/ascii-converter/components/mini-game/asciiToText.js
@@ -1,25 +1,52 @@
-import { Text, View, SafeAreaView, TextInput } from "react-native";
-import React from "react";
+import { Text, View, SafeAreaView, TextInput, Pressable } from "react-native";
+import React, { useState, useEffect } from "react";
 import miniGameStyle from "./miniGameStyle";
 import globalContentStyle from "../../globalStyles/globalContentStyle";
 
 const AsciiToTextGame = () => {
-  const [text, onChangeText] = React.useState("");
-  const [number, onChangeNumber] = React.useState("");
+  const [randomAscii, setRandomAscii] = useState(null);
+  const [userAnswer, setUserAnswer] = useState("");
+  const [result, setResult] = useState("");
+  useEffect(() => {
+    generateRandomAscii();
+  }, []);
+  const generateRandomAscii = () => {
+    const randomAsciiValue = Math.floor(Math.random() * (125 - 33 + 1)) + 33; // Zakres od 33 do 125
+    setRandomAscii(randomAsciiValue);
+    setUserAnswer("");
+    setResult("");
+  };
+
+  const checkAnswer = () => {
+    const inputText = userAnswer.trim();
+    const asciiValue = inputText.length === 1 ? inputText.charCodeAt(0) : NaN;
+    if (!isNaN(asciiValue) && asciiValue === randomAscii) {
+      setResult("Correct!");
+    } else {
+      setResult("Incorrect. Try again.");
+    }
+  };
+
   return (
     <View style={miniGameStyle.tabContentStyle}>
       <Text style={miniGameStyle.label}>Guess the text:</Text>
-      <View style={globalContentStyle.outputContainer}></View>
+      <View style={globalContentStyle.outputContainer}>
+        <Text>{randomAscii}</Text>
+      </View>
       <Text style={miniGameStyle.label}>Your answer:</Text>
       <SafeAreaView style={miniGameStyle.safeAreaView}>
         <TextInput
-          onChangeText={onChangeNumber}
-          value={number}
-          keyboardType="numeric"
+          onChangeText={setUserAnswer}
+          value={userAnswer}
+          keyboardType="ascii-capable"
           style={globalContentStyle.input}
           maxLength={3}
         />
       </SafeAreaView>
+      <Pressable style={miniGameStyle.convertButton} onPress={checkAnswer}>
+        <Text style={miniGameStyle.buttonText}>Check Answer</Text>
+      </Pressable>
+      <Text style={miniGameStyle.result}>{result}</Text>
     </View>
   );
 };
